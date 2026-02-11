17d74b3b8e41aed90a8064e481c7feb74cde0e9c
Pam S <pamela.selle@gmail.com>
2026-02-11 17:32:23 -0500

    feat: design (#26)
    * Start on v2 homepage
    
    * Add wallet buttons, footer
    
    * CSS work
    
    * More CSS
    
    * Replace Home w v2
    
    * Remove design system, unwanted files
    
    * Add cool background, via https://www.magicpattern.design/tools/css-backgrounds
    
    * Adjust background to fixed image status
    
    * Work buy button into design
    
    * Tweak paragraph
    
    * Format index, change google font, change title font
    
    * Animation for info block

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,13 @@
-import { Layout } from "@stellar/design-system";
 import "./App.module.css";
-import ConnectAccount from "./components/ConnectAccount.tsx";
 import { Routes, Route, Outlet } from "react-router-dom";
 import Home from "./pages/Home";
+import styles from "./App.module.css";
 
-const AppName = "Important Diagrams";
 const AppLayout: React.FC = () => (
-  <main>
-    <Layout.Header
-      projectId={AppName}
-      projectTitle={AppName}
-      contentRight={
-        <>
-          <ConnectAccount />
-        </>
-      }
-    />
+  <>
+    <div className={styles.backgroundPattern}></div>
     <Outlet />
-    <Layout.Footer hideLegalLinks={true}>
-      <span>
-        © {new Date().getFullYear()} {AppName}. Licensed under the{" "}
-        <a
-          href="http://www.apache.org/licenses/LICENSE-2.0"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Apache License, Version 2.0
-        </a>
-        .
-      </span>
-    </Layout.Footer>
-  </main>
+  </>
 );
 
 function App() {
