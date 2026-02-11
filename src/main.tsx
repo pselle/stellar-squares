31203fd574e40ae469b4eea23254813969d1ff3d
Pam Selle <pamela.selle@gmail.com>
2026-02-11 14:23:03 -0500

    Animation for info block

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.tsx";
 import { WalletProvider } from "./providers/WalletProvider.tsx";
 import { BrowserRouter } from "react-router-dom";
